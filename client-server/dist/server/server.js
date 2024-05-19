94bbb158191caa55ae70c6aa1cfed75b56e7701b
gabriwl165 <gabrielmarcelino165@gmail.com>
2024-05-19 20:56:17 -0300

    fix: finally finished the compile for gRPC for server side

--- client-server/dist/server/server.js
+++ client-server/dist/server/server.js
@@ -1,11 +1,13 @@
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ServerPing = void 0;
+const ping_pb_js_1 = require("../../protos/ping_pb.js");
 // grpc.handleUnaryCall<ping_pb.Ping, ping_pb.Pong>
-import {Pong} from "../../protos/ping_pb.js";
-
-export class ServerPing {
+class ServerPing {
     ping(call, callback) {
-        const response = new Pong();
+        const response = new ping_pb_js_1.Pong();
         response.setMessage(`Response from Server: ${call.request.getMessage()}`);
         callback(null, response);
     }
 }
+exports.ServerPing = ServerPing;
