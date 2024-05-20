d482cc913cc22df4a139fa0b04645679d9f7cc85
gabriwl165 <gabrielmarcelino165@gmail.com>
2024-05-19 21:05:55 -0300

    chore: gambiarra para buildar o proto no npm run dev

---
+++ client-server/build.sh
@@ -0,0 +1,3 @@
+cd protos/
+../node_modules/.bin/grpc_tools_node_protoc --grpc_out=grpc_js:/home/gabs/Documents/projetos/proxy-server-client/client-server/protos --js_out=import_style=commonjs,binary:/home/gabs/Documents/projetos/proxy-server-client/client-server/protos ping.proto
+protoc --plugin=protoc-gen-ts=/home/gabs/Documents/projetos/proxy-server-client/client-server/node_modules/.bin/protoc-gen-ts --ts_out=grpc_js:/home/gabs/Documents/projetos/proxy-server-client/client-server/protos ping.proto
